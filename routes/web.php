c3134f91a7b19df53cc5b2047c99289555f3bc35
Stanislav Shein <firmvw@gmail.com>
2023-08-30 22:26:31 +0300

    add laravel/breeze, mailtrap.io, laracasts/flash

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('main');
+});
 
-Route::get('/tasks', function () {
-    return view('tasks');
-})->name('tasks');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/task_statuses', function () {
-    return view('task_statuses');
-})->name('task_statuses');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/labels', function () {
-    return view('labels');
-})->name('labels');
+require __DIR__.'/auth.php';
