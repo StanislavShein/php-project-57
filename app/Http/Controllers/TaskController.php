1ecc3efc58347d2f77a3c4c359ee79d57b263de2
Stanislav Shein <firmvw@gmail.com>
2023-09-15 19:50:28 +0300

    add LabelController and views

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
-use App\Models\Taskstatus;
+use App\Models\TaskStatus;
 use App\Models\User;
 use App\Models\Label;
 use Illuminate\Http\Request;
