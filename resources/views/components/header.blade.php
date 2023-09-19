efa27b55271de0610cd33472074e8cf6d97f59d7
Stanislav Shein <firmvw@gmail.com>
2023-09-19 18:47:00 +0300

    add LabelsTest and flash

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,19 +5,17 @@
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ __('layouts.header.appname') }}</span>
             </a>
             <div class="flex items-center lg:order-2">
-            @if (Route::has('login'))
-        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-            @auth
-            <a href="{{ route('logout') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-method="post" rel="nofollow">{{ __('layouts.header.logout') }}</a>
-            @else
-            <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('layouts.header.login') }}</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('layouts.header.registration') }}</a>
-            @endif
-            @endauth
-        </div>
-        @endif
+                @if (Route::has('login'))
+                @auth
+                    <a href="{{ route('logout') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-method="post" rel="nofollow">{{ __('layouts.header.logout') }}</a>
+                @else
+                    <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('layouts.header.login') }}</a>
+                
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">{{ __('layouts.header.registration') }}</a>
+                @endif
+                @endauth
+                @endif
             </div>
             <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
